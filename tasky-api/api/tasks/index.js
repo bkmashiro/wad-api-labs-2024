69f5fb5dff9c1ffe67e0845d1a655926c40e0e34
bkmashiro <53376445+bkmashiro@users.noreply.github.com>
2024-11-29 22:14:37 +0000

    Mongo lab Error Handling

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -1,30 +1,31 @@
 import express from 'express';
 import Task from './taskModel';
+import asyncHandler from 'express-async-handler';
 
 const router = express.Router(); // eslint-disable-line
 
 // Get all tasks
 router.get('/', async (req, res) => {
-    const tasks = await Task.find();
-    res.status(200).json(tasks);
+  const tasks = await Task.find();
+  res.status(200).json(tasks);
 });
 
 // create a task
-router.post('/', async (req, res) => {
-    const task = await Task(req.body).save();
-    res.status(201).json(task);
-});
+router.post('/', asyncHandler(async (req, res) => {
+  const task = await Task(req.body).save();
+  res.status(201).json(task);
+}));
 
 // Update Task
 router.put('/:id', async (req, res) => {
   if (req.body._id) delete req.body._id;
   const result = await Task.updateOne({
-      _id: req.params.id,
+    _id: req.params.id,
   }, req.body);
   if (result.matchedCount) {
-      res.status(200).json({ code:200, msg: 'Task Updated Successfully' });
+    res.status(200).json({ code: 200, msg: 'Task Updated Successfully' });
   } else {
-      res.status(404).json({ code: 404, msg: 'Unable to find Task' });
+    res.status(404).json({ code: 404, msg: 'Unable to find Task' });
   }
 });
 
@@ -32,12 +33,12 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   if (req.body._id) delete req.body._id;
   const result = await Task.deleteOne({
-      _id: req.params.id,
+    _id: req.params.id,
   });
   if (result.deletedCount) {
-      res.status(204).json();
+    res.status(204).json();
   } else {
-      res.status(404).json({ code: 404, msg: 'Unable to find Task' });
+    res.status(404).json({ code: 404, msg: 'Unable to find Task' });
   }
 });
 
